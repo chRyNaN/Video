02eddf5b344c7ccdc0892c6a148e8f10b7b14059
Chris <ckeenan@starry.com>
2019-03-22 13:27:13 -0400

    Initialized StringResourceAccessor for Android App Module

--- android/src/main/java/com/chrynan/video/ui/VideoApplication.kt
+++ android/src/main/java/com/chrynan/video/ui/VideoApplication.kt
@@ -1,9 +1,17 @@
 package com.chrynan.video.ui
 
+import com.chrynan.strings.AndroidStringResourceAccessor
+import com.chrynan.strings.Strings
 import com.chrynan.video.di.component.DaggerAppComponent
 import dagger.android.support.DaggerApplication
 
 class VideoApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Strings.accessor = AndroidStringResourceAccessor(appContext = this)
+    }
 
     override fun applicationInjector() =
         DaggerAppComponent.builder()
