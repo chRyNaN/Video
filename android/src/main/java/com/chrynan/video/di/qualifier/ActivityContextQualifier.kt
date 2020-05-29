34b558722cbac15bd27931c0e2d00e6c9941c442
Chris <ckeenan@starry.com>
2020-05-29 18:57:08 -0400

    Created ActivityContextQualifier and ApplicationContextQualifier

---
+++ android/src/main/java/com/chrynan/video/di/qualifier/ActivityContextQualifier.kt
@@ -0,0 +1,15 @@
+package com.chrynan.video.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+annotation class ActivityContextQualifier
