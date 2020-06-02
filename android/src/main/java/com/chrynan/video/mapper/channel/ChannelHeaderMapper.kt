5639f8090d1e3b4c762ef48ca17c221b99c1caff
Chris <ckeenan@starry.com>
2020-06-02 15:12:39 -0400

    Created ChannelHeaderMapper

---
+++ android/src/main/java/com/chrynan/video/mapper/channel/ChannelHeaderMapper.kt
@@ -0,0 +1,20 @@
+package com.chrynan.video.mapper.channel
+
+import com.chrynan.common.model.ChannelResult
+import com.chrynan.video.mapper.Mapper
+import com.chrynan.video.viewmodel.ChannelHeaderViewModel
+import javax.inject.Inject
+
+class ChannelHeaderMapper @Inject constructor() : Mapper<ChannelResult, ChannelHeaderViewModel> {
+
+    override suspend fun map(model: ChannelResult): ChannelHeaderViewModel =
+        ChannelHeaderViewModel(
+            channelId = model.channel.id,
+            providerUri = model.provider.uri,
+            name = model.channel.name,
+            subscriberCount = model.channel.count.totalSubscribers.toString(), // TODO update
+            totalVideoViewCount = model.channel.count.totalVideoViews.toString(), // TODO update
+            channelHeaderImage = model.channel.images.banner,
+            channelImage = model.channel.images.thumbnail
+        )
+}
---
+++ common/src/commonMain/kotlin/com.chrynan.common/model/ChannelResult.kt
@@ -0,0 +1,6 @@
+package com.chrynan.common.model
+
+data class ChannelResult(
+    val channel: Channel,
+    val provider: Provider
+)
