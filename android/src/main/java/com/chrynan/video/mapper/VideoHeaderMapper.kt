5e17224f5b0822166b80bbddbccc1e30f7655300
Chris <ckeenan@starry.com>
2020-06-02 14:38:28 -0400

    Created VideoHeaderMapper

---
+++ android/src/main/java/com/chrynan/video/mapper/VideoHeaderMapper.kt
@@ -0,0 +1,23 @@
+package com.chrynan.video.mapper
+
+import com.chrynan.common.model.Video
+import com.chrynan.common.model.VideoResult
+import com.chrynan.video.viewmodel.VideoInfoHeaderViewModel
+import javax.inject.Inject
+
+class VideoHeaderMapper @Inject constructor(
+    private val actionsMapper: VideoActionsMapper,
+    private val providerMapper: VideoProviderMapper
+) : Mapper<VideoResult, VideoInfoHeaderViewModel> {
+
+    override suspend fun map(model: VideoResult): VideoInfoHeaderViewModel =
+        VideoInfoHeaderViewModel(
+            videoInfo = model.info,
+            title = model.video.name,
+            detail = getDetailText(model.video),
+            actions = actionsMapper.map(model),
+            provider = providerMapper.map(model)
+        )
+
+    private fun getDetailText(video: Video): String = ""
+}
