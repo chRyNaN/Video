58a480a1fbb5c2b17c191c8530339335621f088f
Chris <ckeenan@starry.com>
2020-06-04 23:23:36 -0400

    Updated ServiceRepository

--- android/src/main/java/com/chrynan/video/database/source/ServiceRepositorySource.kt
+++ android/src/main/java/com/chrynan/video/database/source/ServiceRepositorySource.kt
@@ -1,6 +1,7 @@
 package com.chrynan.video.database.source
 
 import com.chrynan.common.model.Service
+import com.chrynan.common.model.core.UriString
 import com.chrynan.common.repository.ServiceRepository
 import com.chrynan.video.database.dao.DbServiceDao
 import com.chrynan.video.database.mapper.DbServiceFromServiceMapper
@@ -14,4 +15,24 @@ class ServiceRepositorySource @Inject constructor(
 ) : ServiceRepository {
 
     override suspend fun getAll(): List<Service> = dao.getAll().map { toServiceMapper.map(it) }
+
+    override suspend fun getByProviderUri(providerUri: UriString): Service? =
+        dao.getByProviderUri(providerUri)?.let { toServiceMapper.map(it) }
+
+    override suspend fun insert(model: Service) {
+        val dbModel = toDbServiceMapper.map(model)
+
+        dao.insert(dbModel)
+    }
+
+    override suspend fun update(model: Service) {
+        val dbModel = toDbServiceMapper.map(model)
+
+        dao.update(dbModel)
+    }
+
+    override suspend fun deleteByProviderUri(providerUri: UriString) =
+        dao.deleteByProviderUri(providerUri)
+
+    override suspend fun deleteAll() = dao.deleteAll()
 }
