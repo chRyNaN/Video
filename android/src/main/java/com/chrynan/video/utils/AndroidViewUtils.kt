853cd67f6f51c57355b6b45f9105c7dcd65b1fb7
Chris <ckeenan@starry.com>
2020-06-06 01:41:59 -0400

    Created TextView.textChanges() function

--- android/src/main/java/com/chrynan/video/utils/AndroidViewUtils.kt
+++ android/src/main/java/com/chrynan/video/utils/AndroidViewUtils.kt
@@ -1,9 +1,16 @@
 package com.chrynan.video.utils
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.TextView
 import com.chrynan.video.ui.view.SnackbarView
 import com.chrynan.video.R
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.onCompletion
 
 fun snackbarOf(
     view: View?,
@@ -43,4 +50,24 @@ fun snackbarOf(
             )
         }
     }
+}
+
+fun TextView.textChanges(): Flow<CharSequence?> {
+    val mutableStateFlow = MutableStateFlow(text)
+
+    val textWatcher = object : TextWatcher {
+
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            mutableStateFlow.value = s
+        }
+    }
+
+    addTextChangedListener(textWatcher)
+
+    return mutableStateFlow
+        .onCompletion { removeTextChangedListener(textWatcher) }
 }
