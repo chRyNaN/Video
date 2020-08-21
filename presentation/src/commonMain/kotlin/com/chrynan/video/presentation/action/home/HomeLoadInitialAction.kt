ac8a412f370039dd18d729d95b35031e70ceedd4
Chris <ckeenan@starry.com>
2020-08-20 21:50:59 -0400

    Implemented HomeLoadInitialAction and HomeLoadMoreAction

--- presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/home/HomeLoadInitialAction.kt
+++ presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/home/HomeLoadInitialAction.kt
@@ -1,14 +1,29 @@
 package com.chrynan.video.presentation.action.home
 
 import com.chrynan.video.common.Inject
+import com.chrynan.video.common.model.api.FeedItem
+import com.chrynan.video.common.repository.FeedItemRepository
+import com.chrynan.video.common.utils.startWith
 import com.chrynan.video.presentation.action.Action
+import com.chrynan.video.presentation.mapper.video.VideoShowcaseMapper
 import com.chrynan.video.presentation.state.HomeChange
 import com.chrynan.video.presentation.state.HomeIntent
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
-class HomeLoadInitialAction @Inject constructor() : Action<HomeIntent.LoadInitial, HomeChange> {
+class HomeLoadInitialAction @Inject constructor(
+    private val feedItemRepository: FeedItemRepository,
+    private val mapper: VideoShowcaseMapper
+) : Action<HomeIntent.LoadInitial, HomeChange> {
 
-    operator fun invoke(intent: HomeIntent.LoadInitial): Flow<HomeChange> {
-        TODO("Not yet implemented")
-    }
+    operator fun invoke(intent: HomeIntent.LoadInitial): Flow<HomeChange> =
+        feedItemRepository.get()
+            .filterNotNull()
+            .map { items ->
+                items.filterIsInstance<FeedItem<FeedQuery.AsVideoFeedItem>>()
+                    .map { mapper.map(it) }
+            }
+            .map { HomeChange.Loaded(items = it) }
+            .startWith(HomeChange.StartedLoading())
 }
--- presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/home/HomeLoadMoreAction.kt
+++ presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/home/HomeLoadMoreAction.kt
@@ -1,18 +1,36 @@
 package com.chrynan.video.presentation.action.home
 
 import com.chrynan.video.common.Inject
+import com.chrynan.video.common.model.api.FeedItem
+import com.chrynan.video.common.repository.FeedItemRepository
+import com.chrynan.video.common.utils.startWith
 import com.chrynan.video.presentation.action.Action
+import com.chrynan.video.presentation.mapper.video.VideoShowcaseMapper
 import com.chrynan.video.presentation.state.HomeChange
 import com.chrynan.video.presentation.state.HomeIntent
 import com.chrynan.video.presentation.viewmodel.AdapterItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
-class HomeLoadMoreAction @Inject constructor() : Action<HomeIntent.LoadMore, HomeChange> {
+@OptIn(ExperimentalCoroutinesApi::class)
+class HomeLoadMoreAction @Inject constructor(
+    private val feedItemRepository: FeedItemRepository,
+    private val mapper: VideoShowcaseMapper
+) : Action<HomeIntent.LoadMore, HomeChange> {
 
     operator fun invoke(
         intent: HomeIntent.LoadMore,
         currentItems: List<AdapterItem>
-    ): Flow<HomeChange> {
-        TODO("Not yet implemented")
-    }
+    ): Flow<HomeChange> = feedItemRepository.get()
+        .onStart { feedItemRepository.loadMore() }
+        .filterNotNull()
+        .map { items ->
+            items.filterIsInstance<FeedItem<FeedQuery.AsVideoFeedItem>>()
+                .map { mapper.map(it) }
+        }
+        .map { HomeChange.Loaded(items = it) }
+        .startWith(HomeChange.StartedLoading(currentItems = currentItems))
 }
