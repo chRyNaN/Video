792f95db8fd9f93a5b4a4757ed9ad833a16ecf5f
Chris <ckeenan@starry.com>
2020-08-26 20:24:05 -0400

    Implemented SearchReducer

--- presentation/src/commonMain/kotlin/com/chrynan/video/presentation/reducer/SearchReducer.kt
+++ presentation/src/commonMain/kotlin/com/chrynan/video/presentation/reducer/SearchReducer.kt
@@ -4,9 +4,23 @@ import com.chrynan.video.common.Inject
 import com.chrynan.video.presentation.state.SearchChange
 import com.chrynan.video.presentation.state.SearchState
 
-class SearchReducer @Inject constructor(): Reducer<SearchState, SearchChange> {
+class SearchReducer @Inject constructor() : Reducer<SearchState, SearchChange> {
 
-    override suspend fun reduce(previous: SearchState, change: SearchChange): SearchState {
-        TODO("Not yet implemented")
-    }
+    override suspend fun reduce(previous: SearchState, change: SearchChange): SearchState =
+        when {
+            change is SearchChange.Cleared -> SearchState.DisplayingNoInput
+            change is SearchChange.Loaded && change.items.isEmpty() -> SearchState.DisplayingEmpty(
+                query = change.query
+            )
+            change is SearchChange.Loaded -> SearchState.DisplayingLoaded(
+                query = change.query,
+                items = change.items
+            )
+            change is SearchChange.StartedSearching -> SearchState.Searching(query = change.query)
+            previous is SearchState.DisplayingLoaded && change is SearchChange.StartedLoading -> SearchState.LoadingMore(
+                query = change.query,
+                currentItems = change.currentItems
+            )
+            else -> previous
+        }
 }
