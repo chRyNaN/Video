0b02e1f298ae1dac41dcce5d5b31276dcae786bd
Chris <ckeenan@starry.com>
2020-08-26 21:32:24 -0400

    Implemented SearchClearAction

--- presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/search/SearchClearAction.kt
+++ presentation/src/commonMain/kotlin/com/chrynan/video/presentation/action/search/SearchClearAction.kt
@@ -5,10 +5,10 @@ import com.chrynan.video.presentation.action.Action
 import com.chrynan.video.presentation.state.SearchChange
 import com.chrynan.video.presentation.state.SearchIntent
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 class SearchClearAction @Inject constructor() : Action<SearchIntent.Clear, SearchChange> {
 
-    override fun perform(intent: SearchIntent.Clear): Flow<SearchChange> {
-        TODO("Not yet implemented")
-    }
+    override fun perform(intent: SearchIntent.Clear): Flow<SearchChange> =
+        flowOf(SearchChange.Cleared)
 }
